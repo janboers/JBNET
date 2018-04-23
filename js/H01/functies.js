f40b305183e830ac5eebe621d23addf0d74ceefb
Jan <jboers@jbnet.nl>
2018-04-23 20:41:29 +0200

    opschonen

--- js/H01/functies.js
+++
@@ -1,6 +0,0 @@
-// functie
-function toonPrompt(){
-	var code = prompt('Vul uw promotiecode in', 'uw code');
-	var tekst = 'De code die u invoerde was: ' + code;
-	document.getElementById('divResult').innerHTML = tekst;
-}
